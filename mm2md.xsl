f56ece05453c1d78804fb5e78c8e82dcf001a834
juanlealz <juanlealz@gmail.com>
2014-09-16 19:13:45 +0200

    Explicit sections and bullets

---
+++ mm2md.xsl
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="UTF-8"?>
+
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+  <xsl:output method="text" indent="no" encoding="ISO-8859-1"/>
+  <xsl:strip-space elements="*"/>
+  <xsl:key name="refid" match="node" use="@ID"/>
+
+<xsl:variable name="tab" select="'    '"/>
+
+<!-- Ignore subtree comments -->
+<xsl:template match="node[@COLOR='#cc6600']">
+</xsl:template>
+
+<!-- Ignore single-node comments -->
+<xsl:template match="node[@COLOR='#a6a6a6']">
+    <xsl:apply-templates/>
+</xsl:template>
+
+<!-- Document title -->
+<xsl:template match="/map/node">
+    <xsl:text>---
+title: </xsl:text>
+    <xsl:apply-templates select="." mode="text"/>
+    <xsl:text>
+...
+
+</xsl:text>
+    <xsl:apply-templates/>
+</xsl:template>
+
+<!-- Headers -->
+<xsl:template match="node[@COLOR='#000001']">
+    <xsl:for-each select="ancestor::node()/attribute::COLOR">
+        <xsl:if test=".='#000001'">
+            <xsl:text>#</xsl:text>
+        </xsl:if>
+    </xsl:for-each>
+    <xsl:text># </xsl:text>
+    <xsl:apply-templates select="." mode="text"/>
+    <xsl:text>
+
+</xsl:text>
+    <!-- Continue processing children-->
+    <xsl:apply-templates/>
+</xsl:template>
+
+<!-- Bullet lists -->
+<xsl:template match="node[@COLOR='#000005']">
+    <xsl:apply-templates select="." mode="text"/>
+    <xsl:text>
+
+</xsl:text>
+    <!-- Continue processing children-->
+    <xsl:apply-templates mode="bullet">
+         <xsl:with-param name="bullet" select="'- '"/>
+    </xsl:apply-templates>
+</xsl:template>
+<xsl:template match="node[@COLOR='#000006']">
+    <xsl:apply-templates select="." mode="text"/>
+    <xsl:text>
+
+</xsl:text>
+    <!-- Continue processing children-->
+    <xsl:apply-templates mode="bullet">
+         <xsl:with-param name="bullet" select="'1. '"/>
+    </xsl:apply-templates>
+</xsl:template>
+<xsl:template match="node" mode="bullet">
+    <xsl:param name="indent" />
+    <xsl:param name="bullet" />
+    <xsl:value-of select="$indent"/>
+    <xsl:value-of select="$bullet"/>
+    <xsl:apply-templates select="." mode="text"/>
+    <xsl:text>
+</xsl:text>
+    <!-- Continue processing children-->
+    <xsl:apply-templates mode="bullet">
+         <xsl:with-param name="bullet">
+              <xsl:value-of select="$bullet"/>
+         </xsl:with-param>
+         <xsl:with-param name="indent">
+              <xsl:copy-of select="$tab"/>
+              <xsl:value-of select="$indent"/>
+         </xsl:with-param>
+    </xsl:apply-templates>
+</xsl:template>
+
+<!-- Default nodes -->
+<xsl:template match="node">
+    <xsl:apply-templates select="." mode="text"/>
+    <xsl:text>
+
+</xsl:text>
+</xsl:template>
+
+<!-- Process text inside nodes -->
+<xsl:template match="node[@LINK]" mode="text">  
+    <xsl:text>[</xsl:text>
+    <xsl:value-of select="@TEXT"/>
+    <xsl:text>](</xsl:text>
+    <xsl:value-of select="@LINK"/>
+    <xsl:text>)</xsl:text>
+</xsl:template>
+
+<xsl:template match="node" mode="text">  
+    <!-- Print node text -->
+    <xsl:value-of select="@TEXT"/>
+</xsl:template>
+
+</xsl:stylesheet>
---
+++ test/basic.mm
@@ -0,0 +1,30 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1338011328513" ID="ID_383643333" MODIFIED="1410877802179" TEXT="Basic test">
+<node CREATED="1410877825746" ID="ID_1322877370" MODIFIED="1410877873590" POSITION="right" TEXT="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."/>
+<node COLOR="#000001" CREATED="1410877805213" ID="ID_448788708" MODIFIED="1410878511883" POSITION="right" TEXT="Section 1, explicit bullets">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="list"/>
+<node CREATED="1410877884075" ID="ID_273699350" MODIFIED="1410877922376" TEXT="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."/>
+<node COLOR="#000005" CREATED="1410877922385" ID="ID_1047340057" MODIFIED="1410878098080" TEXT="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.">
+<icon BUILTIN="text_list_bullets"/>
+<node CREATED="1410877928912" ID="ID_1177936739" LINK="http://www.lipsum.com/" MODIFIED="1410878897539" TEXT="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."/>
+<node CREATED="1410877935264" ID="ID_318109866" MODIFIED="1410877935265" TEXT="Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.">
+<node CREATED="1410878152385" ID="ID_249110911" MODIFIED="1410881486065" TEXT="Mauris nec nunc eleifend, finibus justo id, finibus ex.">
+<node CREATED="1410881491927" ID="ID_1755883414" MODIFIED="1410883118337" TEXT="Nam consequat augue in pharetra elementum."/>
+<node CREATED="1410883118343" ID="ID_1259984113" MODIFIED="1410883122841" TEXT="In vel felis vel ipsum malesuada volutpat."/>
+</node>
+<node CREATED="1410881486072" ID="ID_1466353405" MODIFIED="1410881491920" TEXT="Aliquam sapien odio, ultricies eu elit vulputate, tempus lacinia eros."/>
+</node>
+</node>
+<node CREATED="1410878112406" ID="ID_1843364723" MODIFIED="1410878122400" TEXT="Aenean ac diam sed nulla sagittis cursus sed varius lorem."/>
+<node COLOR="#000006" CREATED="1410878122406" ID="ID_1411702957" MODIFIED="1410878180765" TEXT="Nulla mauris elit, egestas vitae egestas et, luctus in arcu.">
+<icon BUILTIN="text_list_numbers"/>
+<node CREATED="1410878132455" ID="ID_1168490958" MODIFIED="1410878140768" TEXT="Pellentesque blandit posuere turpis."/>
+<node CREATED="1410878140769" ID="ID_1498396552" MODIFIED="1410878146272" TEXT="Proin a felis lorem."/>
+<node CREATED="1410878146272" ID="ID_1291811253" MODIFIED="1410878152384" TEXT="Quisque efficitur risus nec rhoncus luctus."/>
+</node>
+<node CREATED="1410883122841" ID="ID_152339967" MODIFIED="1410883122843" TEXT="Vestibulum nisl massa, posuere eu nulla a, vulputate porttitor dui. Pellentesque cursus dui id purus molestie, et auctor purus condimentum. In nisi quam, ullamcorper vel condimentum vel, posuere a ante. Mauris est arcu, feugiat vel metus ac, porttitor bibendum ex."/>
+</node>
+</node>
+</map>
